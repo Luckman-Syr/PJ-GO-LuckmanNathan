d8ec60a0885ae2dbed11bd03cc028e017ec6cabe
Luckman-Syr <luckmannathan46@gmail.com>
2024-03-23 09:26:27 +0700

    change env

--- main.go
+++ main.go
@@ -3,15 +3,9 @@ package main
 import (
 	"project-akhir/database"
 	"project-akhir/routers"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		panic("Failed to load env file")
-	}
 	database.StartDB()
 	r := routers.StartRouter()
 	r.Run()
